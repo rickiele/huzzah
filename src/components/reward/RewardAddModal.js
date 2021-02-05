39d5088a015a6381ea11ec2ab406febd83d51f04
rickiele <rickienthefishies@gmail.com>
2021-02-04 22:07:29 -0600

    rewards can be added, and show up in DB

--- src/components/reward/RewardAddModal.js
+++ src/components/reward/RewardAddModal.js
@@ -1,9 +1,50 @@
 import React from "react"
 import { Modal, Button, Form } from "react-bootstrap"
+import { RewardContext } from "./RewardProvider"
+import { useContext, useEffect, useState } from "react"
+import { useHistory, useParams } from "react-router-dom" 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./Reward.css"
 
-function MyVerticallyCenteredModal(props) {
+const RewardAdd = (props) => {
+  const { addReward } = useContext(RewardContext)
+  const currentUser = parseInt(localStorage.getItem("huzzah_user"))
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const history = useHistory();
+  
+  //for edit, hold on to state of animal in this view
+    const [reward, setReward] = useState({
+      userId: currentUser,
+      name: "",
+      location: "",
+      url: ""
+    })
+
+
+   const handleControlledInputChange = (event) => {
+    // Creating an object called newReward
+     const newReward = { ...reward }
+
+     newReward[event.target.id] = event.target.value
+     setReward(newReward)
+   }
+
+
+   const handleSaveReward = () => {
+      setIsLoading(true);
+        //POST - add
+        addReward({
+            id: reward.id,
+            userId: currentUser,
+            name: reward.name,
+            location: reward.location,
+            url: reward.url
+        })
+        .then(() => history.push("/"))
+    }
+
   return (
     <Modal
       {...props}
@@ -18,20 +59,36 @@ function MyVerticallyCenteredModal(props) {
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Group controlId="form__addRewardName">
             <Form.Label>
               <h5>Reward Name</h5>
             </Form.Label>
-            <Form.Control type="text" placeholder="Enter new reward name" />
+            <Form.Control type="text" id="name" placeholder="Enter reward name"
+            onChange={handleControlledInputChange} 
+            />
             <Form.Label>
               <h5>Reward Location</h5>
             </Form.Label>
-            <Form.Control type="text" placeholder="Enter location of reward" />
-          </Form.Group>
+            <Form.Control type="text" id="location" placeholder="Enter location of reward" 
+            onChange={handleControlledInputChange} 
+            />
+            <Form.Label>
+              <h5>Reward URL</h5>
+            </Form.Label>
+            <Form.Control type="text" id="url" placeholder="Enter url of reward" 
+            onChange={handleControlledInputChange} 
+            />
+            </Form.Group>
+            {/* <Form.Group controlId="form__addRewardLocation"> */}
+            {/* </Form.Group> */}
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide}>Save New Reward</Button>
+        <Button onClick={event => {
+                event.preventDefault()
+                handleSaveReward()
+                }}>
+                  Save New Reward</Button>
       </Modal.Footer>
     </Modal>
   );
@@ -46,7 +103,7 @@ export const RewardAddModal = () => {
         Add Reward
       </Button>
 
-      <MyVerticallyCenteredModal
+      <RewardAdd
         show={modalShow}
         onHide={() => setModalShow(false)}
       />
--- src/components/reward/RewardProvider.js
+++ src/components/reward/RewardProvider.js
@@ -24,9 +24,32 @@ export const RewardProvider = (props) => {
         .then(getRewards)
     }
 
+    const getRewardById = (id) => {
+        return fetch(`http://localhost:8088/rewards/${id}`)
+            .then(res => res.json())
+    }
+
+    const deleteReward = rewardId => {
+        return fetch(`http://localhost:8088/rewards/${rewardId}`, {
+            method: "DELETE"
+        })
+        .then(getRewards)
+      }
+
+    const updateReward = reward => {
+      return fetch(`http://localhost:8088/rewards/${reward.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(reward)
+      })
+        .then(getRewards)
+    }
+
     return (
         <RewardContext.Provider value={{
-            rewards, getRewards, addReward
+            rewards, getRewards, addReward, getRewardById, deleteReward, updateReward
         }}>
             {props.children}
         </RewardContext.Provider>
